29b6554646c0671db23c3dac43778a63f252105d
stromcer <dannymm12@hotmail.com>
2023-04-12 21:23:50 +0200

    Creada pagina de opciones y el navbar

--- tetris-boilerplate/src/js/routes/routes.js
+++ tetris-boilerplate/src/js/routes/routes.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../views/home";
+import Options from "../views/Options";
+import Layout from "../layout/layout";
 
-const routes = [{
+const routes = [
+  {
     path: "/",
     element: <Home />
-}]
+  },
+  {
+    path:"/options",
+    element:<Options />
+  }
+]
 
-  const router = createBrowserRouter(routes);
+
+
+
+
+  
+  const fullRoutes = [{
+    path: "/",
+    element: <Layout/>,
+    children: routes
+
+  }]
+
+  const router = createBrowserRouter(fullRoutes);
   export default router
