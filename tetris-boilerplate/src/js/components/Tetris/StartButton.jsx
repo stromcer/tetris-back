96b74356aaf66519c3c5ba241455db0a68ed6478
stromcer <dannymm12@hotmail.com>
2023-05-10 19:28:39 +0200

    solo back

--- tetris-boilerplate/src/js/components/Tetris/StartButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const StartButton = ({ callback }) => (
-  <button className="tetris-startbutton" onClick={callback}>Start Game</button>
-)
-
-export default StartButton;
